8db78876e1ab422c0e31735b42f92258f687c535
Denis Grant <denisgrant2@gmail.com>
2018-12-27 01:55:34 +0000

    commmit

--- index.php
+++ index.php
@@ -164,21 +164,28 @@ get_header();
 <!--// end row -->
 
 <div class="row">
-	<!-- Latest Products -->
-	<div class="col-sm-4 sm-margin-b-50">
-		<div class="margin-b-20">
-			<div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
-				<img class="img-responsive" src="<?php echo get_theme_file_uri('/img/970x647/01.jpg')?>" alt="Latest Products Image">
+<?php if ( have_posts() ) : 
+	while ( have_posts() ) : the_post(); ?>
+		<!-- Latest Products -->
+		<div class="col-sm-4 sm-margin-b-50">
+			<div class="margin-b-20">
+				<div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
+					<!-- <img class="img-responsive" src="<?php echo get_theme_file_uri('/img/970x647/01.jpg')?>" alt="Latest Products Image"> -->
+					<?php the_post_thumbnail( 'large', array (
+						'class' => 'img-responsive'
+					));?>
+				</div>
 			</div>
+			<h4><a href="#">Triangle Roof</a> <span class="text-uppercase margin-l-20">Management</span></h4>
+			<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor</p>
+			<a class="link" href="#">Read More</a>
 		</div>
-		<h4><a href="#">Triangle Roof</a> <span class="text-uppercase margin-l-20">Management</span></h4>
-		<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor</p>
-		<a class="link" href="#">Read More</a>
-	</div>
+	<?php endwhile;
+ endif; ?>
 	<!-- End Latest Products -->
 
 	<!-- Latest Products -->
-	<div class="col-sm-4 sm-margin-b-50">
+	<!-- <div class="col-sm-4 sm-margin-b-50">
 		<div class="margin-b-20">
 			<div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
 				<img class="img-responsive" src="<?php echo get_theme_file_uri('/img/970x647/02.jpg')?>" alt="Latest Products Image">
@@ -187,11 +194,11 @@ get_header();
 		<h4><a href="#">Curved Corners</a> <span class="text-uppercase margin-l-20">Developmeny</span></h4>
 		<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor</p>
 		<a class="link" href="#">Read More</a>
-	</div>
+	</div> -->
 	<!-- End Latest Products -->
 
 	<!-- Latest Products -->
-	<div class="col-sm-4 sm-margin-b-50">
+	<!-- <div class="col-sm-4 sm-margin-b-50">
 		<div class="margin-b-20">
 			<div class="wow zoomIn" data-wow-duration=".3" data-wow-delay=".1s">
 				<img class="img-responsive" src="<?php echo get_theme_file_uri('/img/970x647/03.jpg')?>" alt="Latest Products Image">
@@ -200,7 +207,7 @@ get_header();
 		<h4><a href="#">Bird On Green</a> <span class="text-uppercase margin-l-20">Design</span></h4>
 		<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed tempor incdidunt ut laboret dolor magna ut consequat siad esqudiat dolor</p>
 		<a class="link" href="#">Read More</a>
-	</div>
+	</div> -->
 	<!-- End Latest Products -->
 </div>
 <!--// end row -->
